0416636e6f1b2b2d8352471c059b5754c1691e97
rikewa <rike-wagner@web.de>
2026-04-04 00:20:36 +0200

    coding-log day03

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-
+from tkinter import ttk
+from PIL import Image, ImageTk
 
 root = tk.Tk()
 root.title("Title")
@@ -13,19 +14,18 @@ height = int(sheight * 0.2)
 
 root.geometry(f'{width}x{height}')
 root.minsize(width=200,height=100)
-root.maxsize(width=500, height=400)
+root.maxsize(width=200, height=150)
 root.resizable(True, True)
 
 #root layout
+image = Image.open('logo.png').resize((200,80))
+photo = ImageTk.PhotoImage(image)
 
 #root content 
 label1 = tk.Label(root, text="hello world!", bg='#00ff00')
 label1.pack(side='top', expand=True, fill='both')
 
-label2 = tk.Label(root, text='hallo welt!', bg='#ff00fc')
-label2.pack(side='top', expand=True, fill='both')
-
-
-
+label2 = ttk.Label(root, image=photo)
+label2.pack()
 # --- Start Programm ---
 root.mainloop()
